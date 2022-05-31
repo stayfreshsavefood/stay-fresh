ab5c8236a54d730ba1b30c9f07898543df6ca6d9
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-05-31 16:46:43 +0200

    created ingredient model

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,6 +1,7 @@
 class Fridge < ApplicationRecord
   has_many :fridge_users
   has_many :users, through: :fridge_users
+  has_many :ingredients, dependent: :destroy
 
   validates :name, presence: true
   validates :users, presence: true
