af49ea58964d73f7cf158118b06b6bf005723a1e
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-05-31 18:06:43 +0200

    db seeded with 3 users, 3 fridge and 30 ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,5 +2,5 @@ class Ingredient < ApplicationRecord
   belongs_to :fridge
   has_one :expiry_notification, dependent: :destroy
 
-  validate :name, :exp_date, :category, :quantity, :unit, :comment, :fridge, presence: true
+  validates :name, :exp_date, :category, :quantity, :unit, :fridge, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :username, presence: true
-  has_many :fridge_users
+  has_many :fridge_users, dependent: :destroy
   has_many :fridges, through: :fridge_users
   has_many :bookmarks
 end
