d44cb0151f503fdae3045e12273e32714ff4f256
Hamza Shah Gilani <hamzashah_g@hotmail.com>
2022-06-01 18:08:37 +0200

    adds the search bar

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,11 @@ class Ingredient < ApplicationRecord
   has_one :expiry_notification, dependent: :destroy
 
   validates :name, :exp_date, :category, :quantity, :unit, :fridge, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name, :category ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
