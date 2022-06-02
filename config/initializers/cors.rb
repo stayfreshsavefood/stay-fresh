91d89e058cb32a39ec8bf42bf52cedf43b61f3d6
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-06-03 01:36:45 +0200

    not done

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://shelf-life-api.herokuapp.com'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins 'http://localhost:3000/'
+    resource '*',
+      headers: :any,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end
